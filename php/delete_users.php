8a91ac878e8906f9213ea52d994edb834f0500e3
Aldoptma0704 <aldopratama0707@gmail.com>
2024-06-14 18:34:05 +0700

    delet di admin dan db baru

--- php/delete_users.php
+++ php/delete_users.php
@@ -1,10 +1,10 @@
 <?php
 include 'Koneksi.php';
 
-if (isset($_GET['id_users'])){
-    $id_produk = $_GET['id_users'];
+if (isset($_GET['id'])){
+    $id_produk = $_GET['id'];
 
-    $stmt = $conn->prepare("DELETE FROM users WHERE id_users = ?");
+    $stmt = $conn->prepare("DELETE FROM users WHERE id = ?");
     $stmt->bind_param("i", $id_produk);
     if ($stmt->execute()){
         echo "Akun user berhasil dihapus";
